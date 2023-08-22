755308cbe79bbefb392248d62c691860b44382d4
felipesanderp <felipe.panisson@hotmail.com>
2023-08-22 10:59:59 -0300

    serach form

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,13 +1,38 @@
+import { useForm } from 'react-hook-form'
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { MagnifyingGlass } from 'phosphor-react'
 
 import { SearchFormContainer } from "./styles";
 
-export function SearchForm() {
-  return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
+const searchFormSchema = zod.object({
+  query: zod.string(),
+})
 
-      <button type='submit'>
+export function SearchForm() {
+  const { 
+    register, 
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm<zod.infer<typeof searchFormSchema>>({
+    resolver: zodResolver(searchFormSchema)
+  })
+
+  async function handleSearchTransactions(data: zod.infer<typeof searchFormSchema>) {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    
+    console.log(data)
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input 
+        type="text" 
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+
+      <button type='submit' disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
         Buscar
       </button>
